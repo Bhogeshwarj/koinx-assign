d042ffabe52cc8888cc88754ea4dcbf46dcf6314
Bhogeshwar Jadhav <bhogeshwarj@gmail.com>
2024-03-06 06:58:44 +0530

    Added the chart and some routing

---
+++ src/components/AdandTrend.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import adimg from "../assets/image (1).png"
+
+const AdandTrend = () => {
+  return (
+    <>
+<div className="ml-5 min-h-screen">
+    <div className="card1 text-white flex items-center justify-center flex-col text-center">
+        <h2 className="text-2xl font-bold my-2">Get Started with Coinx for free</h2>
+        <p className="mb-4 px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati atque nobis maxime quos numquam esse, sequi corrupti, odio minima cupiditate ipsam? Impedit, quam eos! Soluta!</p>
+        <img src={adimg} alt="" className="mb-1" />
+        <button className='btn1  text-black px-4 pb-2 '>Get Started for Free </button>
+    </div>
+    <div className="mt-4 text-1xl font-semibold card2">
+        Trending Coins (24h)
+    </div>
+</div>
+
+    </>
+  )
+}
+
+export default AdandTrend
--- src/components/Currencylayout.jsx
+++ src/components/Currencylayout.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import TradingViewWidget from './TradingViewWidget'
+import AdandTrend from './AdandTrend'
+
 const Currencylayout = () => {
   return (
-    <div className=''>CurrencyLayout
+    <>
+    <div className="container mt-5 mx-10 ">
+    <div className=" my-3"> <span className='cl'> Cryptocurrencies &gt;&gt;</span> Bitcoin</div>
+       <div className="mainsection flex justify-between">
+            <div className="flex flex-col chartandinfo">
+                    <div className='currencyInfo chart'>
+                        <TradingViewWidget />
+                    </div>
+            </div>
+            <div className="adandtrend">
+                <AdandTrend/>
+            </div>
+       </div>
     </div>
+    <div className="youmaylike">
+            YouMayAlsoLike
+        </div>
+    </>
   )
 }
 
